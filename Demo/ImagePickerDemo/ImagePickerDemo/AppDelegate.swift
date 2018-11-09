6206d380f5bf56b9bc7f54efce7d380b4f047a5b
farid <farid@info74.ru>
2018-11-09 10:36:46 +0500

    Swift 4.2 support

--- Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
+++ Demo/ImagePickerDemo/ImagePickerDemo/AppDelegate.swift
@@ -1,17 +1,18 @@
 import UIKit
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  lazy var controller: UIViewController = ViewController()
+    lazy var controller: UIViewController = ViewController()
 
-  var window: UIWindow?
+    var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    window = UIWindow()
-    window?.rootViewController = controller
-    window?.makeKeyAndVisible()
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
 
-    return true
-  }
+        return true
+    }
 }
